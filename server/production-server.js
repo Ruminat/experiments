e97178b6e18655d289bb04a2c4d1b1371883db28
Ruminat <Blowlyge@gmail.com>
2022-06-26 17:24:17 +0300

    tupa jino

--- server/production-server.js
+++ server/production-server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const path = require("path");
 
-const PORT = 8080;
+// const PORT = 8080;
+const PORT = 49204;
 const distPath = path.join(__dirname, "..", "dist");
 
 console.log("HOBA!", distPath);
